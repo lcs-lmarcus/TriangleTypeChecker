b3ce0dc9b1abb369d2b8b446a337cc7f4d0e267f
Marcus Li <mmli@lcs.on.ca>
2025-01-20 09:34:07 -0500

    Added stored properties and textfield for sideLength inputs

--- TriangleTypeChecker/Views/CheckerView.swift
+++ TriangleTypeChecker/Views/CheckerView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct CheckerView: View {
+    // MARK: Stored Property
+    // Input of Side Length
+    @State var givenInputA = ""
+    @State var givenInputB = ""
+    @State var givenInputC = ""
+    
+    // Feedback on user
+    @State var feedback = ""
+    
+    // MARK: Computed Property
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                // Input of 3 Sides (Strings)
+                Text ("Enter the Side Length A")
+                TextField("Enter Side Length A", text: $givenInputA)
+                
+                Text ("Enter the Side Length B")
+                TextField("Enter Side Length B", text: $givenInputB)
+                
+                Text ("Enter the Side Length C")
+                TextField("Enter Side Length C", text: $givenInputC)
+                
+            }
+            
+            .padding()
+            .navigationTitle("Triangle Type Checker")
         }
-        .padding()
     }
 }
 
