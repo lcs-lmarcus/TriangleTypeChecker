03d4a99d0f01c8eaf4700742aa79e79c7859bcdb
Marcus Li <mmli@lcs.on.ca>
2025-01-20 09:23:19 -0500

    Initial Project Organization

--- TriangleTypeChecker/Model/TriangleTypeCheckerApp.swift
+++ TriangleTypeChecker/Model/TriangleTypeCheckerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TriangleTypeCheckerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CheckerView()
         }
     }
 }
--- TriangleTypeChecker/Views/CheckerView.swift
+++ TriangleTypeChecker/Views/CheckerView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  CheckerView.swift
 //  TriangleTypeChecker
 //
 //  Created by Marcus Li on 2025-01-20.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CheckerView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    CheckerView()
 }
