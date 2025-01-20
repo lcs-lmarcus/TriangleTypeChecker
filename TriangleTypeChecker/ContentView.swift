4010273ac2589ebe89b74b13d72f9e9591815cc4
Marcus Li <mmli@lcs.on.ca>
2025-01-20 09:03:50 -0500

    Initial Commit

---
+++ TriangleTypeChecker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TriangleTypeChecker
+//
+//  Created by Marcus Li on 2025-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TriangleTypeChecker/TriangleTypeCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriangleTypeCheckerApp.swift
+//  TriangleTypeChecker
+//
+//  Created by Marcus Li on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct TriangleTypeCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
